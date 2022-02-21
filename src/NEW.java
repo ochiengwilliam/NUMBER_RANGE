bfd1bd1a109bd60a760662ef58d478bd88b49726
william79 <ochiengwilliam79@gmail.com>
2022-02-21 16:11:57 +0300

    Initial commit

---
+++ src/NEW.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class NEW {
+    public static void main(String[] args) {
+        Scanner scan  = new Scanner(System.in);
+          int n;
+          do {
+
+              System.out.print("enter a number between 1 and 10: ");
+              n = scan.nextInt();
+          }while
+          (n<1   || n>10);
+
+        System.out.println(n + " is between 1 and 10");
+
+
+
+
+
+     }
+
+}
+
+
